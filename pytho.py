1974d9a4d2001a8df1f315c42708bbefe9aa97b1
zXbusterz <94912377+Jaquelinesb13@users.noreply.github.com>
2022-08-03 21:10:50 -0300

    sc

---
+++ pytho.py
@@ -0,0 +1,15 @@
+from re import A
+from unittest import result
+
+
+def stringSemLetra(letra, palavra):
+    resultado = ''
+    for percorrer in palavra:
+        if(percorrer != letra):
+            resultado = resultado + percorrer
+    return resultado
+
+letraDigitada = input('Digite uma letra:')
+palavraDigitada = input('Digite uma palavra:')
+result = stringSemLetra(letraDigitada.lower(), palavraDigitada.lower())
+print(f'O resultado é {result}')
